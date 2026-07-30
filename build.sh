3ab03d630434148d23141322120df2bcaf6ce238
Imran <imran@example.com>
2026-07-31 03:38:50 +0500

    Install Flutter in build.sh for Vercel deployment

--- build.sh
+++ build.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+set -e
+
+# Install Flutter
+git clone https://github.com/flutter/flutter.git -b stable --depth 1 /tmp/flutter
+export PATH="/tmp/flutter/bin:$PATH"
+
+# Verify Flutter installation
+flutter --version
+
+# Build the web app
 flutter pub get
 flutter build web
